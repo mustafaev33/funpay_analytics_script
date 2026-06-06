a21d3e41dcd7a7efc4edc71bc9e9ac9123ab9a01
Rodion <rdd22833t@gmail.com>
2026-06-06 10:27:44 +0600

    небольшие косметические правки и добавление гистограммы

--- visual.py
+++ visual.py
@@ -5,9 +5,16 @@ from lang_ru import TEXTS
 from lang_en import TEXTS
 
 def visual_data(df, currency):
+
     dates = df['Дата'].apply(lambda x: parse(x, languages=['ru']))
     monthly = df.groupby(dates.dt.to_period('M'))['Цена'].sum()
 
+    plt.style.use('grayscale')
+
+    plt.figure()
     plt.plot(monthly.index.astype(str), monthly.values, marker='o')
+    plt.title(TEXTS['revenue_visual'].format(currency))
+    plt.figure()
+    plt.bar(monthly.index.astype(str), monthly.values, width=0.7)
     plt.title(TEXTS['revenue_visual'].format(currency))
     plt.show()
